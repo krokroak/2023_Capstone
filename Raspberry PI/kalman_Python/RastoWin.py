3b319ac0bf786b86d0dac1fe571071f09a912159
{MinGyu Park} <{kyu3245@gmail.com}>
2023-04-18 16:10:28 +0900

    Change File Name RastoWin.py

--- Raspberry PI/kalman_Python/RastoWin.py
+++ Raspberry PI/kalman_Python/RastoWin.py
@@ -1,7 +1,9 @@
 import time
 import serial
 
-stm = serial.Serial(port= '/dev/ttyAMA1',baudrate=19200,timeout=1.0,bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE)
+stm = serial.Serial(port= '/dev/ttyAMA1',baudrate=19200,timeout=1.0,
+                    bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE)
 time.sleep(2)
 
 num = 1
@@ -10,9 +12,9 @@ while True:
     stm.reset_output_buffer()
     stm.write(power.encode(encoding='UTF-8',errors='ignore'))
     
-    time.sleep(0.3)
+    time.sleep(0.03)
     
     num+=1
     data= stm.read(20)
     print(data.decode(encoding='utf-8',errors='ignore'))
-    time.sleep(0.3)
+    time.sleep(0.03)
